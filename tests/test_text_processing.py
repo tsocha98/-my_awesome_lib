a7f06c47a9782aa1f24e5e8c4acc7d919a57b5d3
tsocha98 <tomaszsocha98@gmail.com>
2025-03-24 07:29:24 +0100

    Update test_text_processing.py

--- tests/test_text_processing.py
+++ tests/test_text_processing.py
@@ -1,14 +1,20 @@
-ECHO is on.
 import unittest
-from my_awesome_lib.text_processing import count_words
+from my_awesome_lib.text_processing import reverse_text
 
 class TestTextProcessing(unittest.TestCase):
-    def test_count_words(self):
-        self.assertEqual(count_words("Hello world"), 2)
-        self.assertEqual(count_words("Python is awesome"), 3)
+    def test_reverse_text(self):
+        self.assertEqual(reverse_text("hello"), "olleh")
+        self.assertEqual(reverse_text("Python"), "nohtyP")
 
-    def test_empty_string(self):
-        self.assertEqual(count_words(""), 0)
+    def test_reverse_text_with_spaces(self):
+        self.assertEqual(reverse_text("hello world"), "dlrow olleh")
+
+    def test_reverse_text_empty_string(self):
+        self.assertEqual(reverse_text(""), "")
+
+    def test_reverse_text_special_characters(self):
+        self.assertEqual(reverse_text("123!@#"), "#@!321")
 
 if __name__ == '__main__':
     unittest.main()
+
