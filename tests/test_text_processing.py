87a770b337de39dd2a3ac0495be29d74614f60c4
tsocha98 <tomaszsocha98@gmail.com>
2025-03-24 07:05:12 +0100

    Update test_text_processing.py

--- tests/test_text_processing.py
+++ tests/test_text_processing.py
@@ -1,1 +1,14 @@
 ECHO is on.
+import unittest
+from my_awesome_lib.text_processing import count_words
+
+class TestTextProcessing(unittest.TestCase):
+    def test_count_words(self):
+        self.assertEqual(count_words("Hello world"), 2)
+        self.assertEqual(count_words("Python is awesome"), 3)
+
+    def test_empty_string(self):
+        self.assertEqual(count_words(""), 0)
+
+if __name__ == '__main__':
+    unittest.main()
