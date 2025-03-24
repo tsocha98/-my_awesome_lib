ec27e418874498a635017a680a7585c8ff58f145
tsocha98 <tomaszsocha98@gmail.com>
2025-03-24 07:04:30 +0100

    Update test_data_utils.py

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,1 +1,18 @@
 ECHO is on.
+import unittest
+from my_awesome_lib.data_utils import convert_to_int
+
+class TestDataUtils(unittest.TestCase):
+    def test_valid_conversion(self):
+        self.assertEqual(convert_to_int("10"), 10)
+        self.assertEqual(convert_to_int("0"), 0)
+
+    def test_invalid_conversion(self):
+        self.assertIsNone(convert_to_int("abc"))
+        self.assertIsNone(convert_to_int("12.3"))
+
+    def test_none_input(self):
+        self.assertIsNone(convert_to_int(None))
+
+if __name__ == '__main__':
+    unittest.main()
