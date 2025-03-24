ce3913a762773b28625a1b995a3a21f934fde4c1
tsocha98 <tomaszsocha98@gmail.com>
2025-03-24 07:04:54 +0100

    Update test_math_tools.py

--- tests/test_math_tools.py
+++ tests/test_math_tools.py
@@ -1,1 +1,14 @@
 ECHO is on.
+import unittest
+from my_awesome_lib.math_tools import add
+
+class TestMathTools(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_large_numbers(self):
+        self.assertEqual(add(1000000, 2000000), 3000000)
+
+if __name__ == '__main__':
+    unittest.main()
